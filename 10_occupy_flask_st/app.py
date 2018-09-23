df2535b2189abb76fcbd11736384a1af1df08e15
Mohammed Uddin <muddin3@stuy.edu>
2018-09-23 15:45:47 -0400

    wrote code

--- 10_occupy_flask_st/app.py
+++ 10_occupy_flask_st/app.py
@@ -1,17 +1,27 @@
+#Undecided -- Mohammed Uddin and Britni Canale
+#SoftDev1 pd6
+#K 06 Jinja Tuning
+#2018-9-21
+
 from flask import Flask, render_template
 
 app=Flask(__name__)
 
 @app.route("/")
 def hello():
-	return ""
+	return """<a href="/occupations"> occupations </a>"""
 
 @app.route("/occupations")
 def ree():
 	with open("data/occupations.csv") as csv_file:
 		import csv
-		reader = csv.DictReader(csv_file, delimiter=',') 
-		return render_template("model_tmplt.html", occupations=reader)
+		reader = csv.DictReader(csv_file, delimiter=',')  #creates list of ordered dictionaries
+		jobList = list(reader)                            #turn reader into a list
+		rList = jobList.copy();                           #copy of job list to select random job
+		rList.pop()                                       #remove last element (total) from possible choices in random
+		import random 
+		randJob=random.choice(rList)                      #random job unweighted
+		return render_template("model_tmplt.html", occupations=jobList, randJob = randJob)
 
 if __name__ == '__main__':
 	app.debug = True
