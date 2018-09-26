5d71768f525ca38c90956063d5d19edb1fe3b3d8
Trollicorn <muddin3@stuy.edu>
2018-09-26 12:33:42 -0400

    wrote stuff

---
+++ makefile
@@ -0,0 +1,5 @@
+all: 
+
+
+venv: 
+	source ~/rock/bin/activate
--- template/app.py
+++ template/app.py
@@ -3,10 +3,12 @@ app=Flask(__name__)
 
 @app.route("/")
 def hello():
-   return ""
+    return ""
 
 @app.route("/my_foist_template")
 def ree():
     return render_template( "model_tmplt.html", foo = "foooo" )
 
+if (__name__=="__main__"):
+    app.debug = True
 app.run()
