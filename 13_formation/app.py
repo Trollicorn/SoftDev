d4b824eb31435742bcabf689b898d174174230c9
Mohammed Uddin <muddin3@stuy.edu>
2018-09-28 01:34:36 -0400

    tried adding work13 but had problems

---
+++ 13_formation/app.py
@@ -0,0 +1,23 @@
+'''
+Mohammed Uddin
+SoftDev1 pd6
+K 13 -- Echo Echo Echo
+2018-09-27
+'''
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")                                
+def home():
+	return render_template("login.html")
+
+@app.route("/auth")                      
+def authenticate():
+	return render_template("authenticate.html", username = request.args['username'])
+
+
+if __name__ == "__main__":
+	app.debug = True
+	app.run()
