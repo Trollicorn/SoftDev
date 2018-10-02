4e0ad7df6bf80a50f5f0f25b143c002c63fb9640
Mohammed Uddin <muddin3@stuy.edu>
2018-10-01 21:29:54 -0400

    fixed typos and added comments

--- 14_login/app.py
+++ 14_login/app.py
@@ -7,29 +7,31 @@ import os
 
 app = Flask(__name__)
 
-app.secret_key = os.urandom(32)
 
+app.secret_key = os.urandom(32)   #super secret key hidden under the doormat
+
+#hardcoded login
 user = 'abc'
 pw = '123'
 
 @app.route("/")
 def login():
-    if "username" in session:
+    if "username" in session:      #if you're logged in already, just go to the welcome page
         return render_template('welcome.html', username= session['username'])
-    return render_template('login.html')
+    return render_template('login.html')  #otherwise login
 
 @app.route("/auth")
 def logged():
-    if request.args['username'] == user and request.args['password'] == pw:
-        session['username'] = request.args['username']
+    if request.args['username'] == user and request.args['password'] == pw:        #if you have the right credentials
+        session['username'] = request.args['username']                             #you get to join the super secret club
         return render_template('welcome.html', username= request.args['username'])
     else:
-        return render_template('error.html')
+        return render_template('error.html')                                       #otherwise you get kicked out
 
 @app.route("/exit")
 def leave():
-    session.pop('username')
-    return redirect(url_for("login"))
+    session.pop('username')                   #you've chosen to leave and head out into the dangerous world
+    return redirect(url_for("login"))         #but you soon come to reget your decision and come back to the login page
 
 
 if __name__== "__main__":
