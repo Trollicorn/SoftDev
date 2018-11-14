d86625b2ed7e4df8c09be6d7e62fa3cb4fff6a9e
Mohammed Uddin <muddin3@stuy.edu>
2018-11-13 20:56:17 -0500

    add the work

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -3,21 +3,18 @@
 #K24 -- A RESTful Journey Skyward
 #2018-11-13
 from flask import Flask, render_template
-import urllib3
+from urllib import request,parse
 import json
 
 app = Flask(__name__)
 
-api_key = "SbfYjFfTeIJ3yt22QfKtIwTYZNMgyofC0jfrPfZf"
-url = "https://api.nasa.gov/planetary/apod?api_key=SbfYjFfTeIJ3yt22QfKtIwTYZNMgyofC0jfrPfZf"
-
-http = urllib3.PoolManager()
-r = http.request('GET',url)
-
-
 @app.route("/")
 def show():
-    return render_template("templates/demo.html")
+    url = "https://api.nasa.gov/planetary/apod?date=2017-10-29&api_key=SbfYjFfTeIJ3yt22QfKtIwTYZNMgyofC0jfrPfZf"
+    book = request.urlopen(url) #open the book
+    stuff = book.read()         #read the stuff in the book
+    things = json.loads(stuff)   #turn the stuff into into things you can understand
+    return render_template("demo.html",title=things['title'],pic=things['url'],explanation=things['explanation'])
 
 
 if __name__== "__main__":
