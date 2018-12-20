1e0463ec1aea95cf93ff98d3d654662956f08b78
Trollicorn <muddin3@stuy.edu>
2018-12-20 12:33:26 -0500

    words get added

---
+++ 30_js/dommy.js
@@ -0,0 +1,17 @@
+//[] -- Mohammed Uddin and Isaac Jon
+//
+//
+//
+
+var addItem = function(e) {
+	var list = document.getElementById("thelist");	
+	var item = document.createElement("li");
+	item.innerHTML = "WORD";
+	list.appendChild(item);
+};
+
+
+var button = document.getElementById("b");
+button.addEventListener('click',addItem);
+
+
