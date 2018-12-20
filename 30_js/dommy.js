7e15c1ed3f5c22bf511951f11be01a28ef87bb95
Trollicorn <muddin3@stuy.edu>
2018-12-20 16:57:28 -0500

    finished basic work 30

--- 30_js/dommy.js
+++ 30_js/dommy.js
@@ -3,15 +3,58 @@
 //
 //
 
+var changeHeading = function(e) {
+	h.innerHTML = this.innerHTML;
+};
+
+var removeItem = function(e) {
+	this.remove();
+};
+
+var lis = document.getElementsByTagName("li");
+
+for (var i = 0; i<lis.length; i++){
+	lis[i].addEventListener('mouseover',changeHeading);
+	lis[i].addEventListener('mouseout',function(){h.innerHTML="Hello World!"});
+	lis[i].addEventListener('click',removeItem);
+}
+
 var addItem = function(e) {
 	var list = document.getElementById("thelist");	
 	var item = document.createElement("li");
 	item.innerHTML = "WORD";
+	item.addEventListener('mouseover',changeHeading);
+	item.addEventListener('mouseout',function(){h.innerHTML="Hello World!"});
+	item.addEventListener('click',removeItem);
 	list.appendChild(item);
 };
 
-
 var button = document.getElementById("b");
 button.addEventListener('click',addItem);
+
+var fib = function(n) {
+	if (n<2){
+		return 1;
+	}
+	else {
+		return fib(n-1) + fib(n-2);
+	}
+};
+
+
+var addFib = function(e) {
+	console.log(e);
+	var list = document.getElementById("fiblist");	
+	var item = document.createElement("li");
+	item.innerHTML = fib(list.childElementCount);
+	list.appendChild(item);
+};
+
+var addFib2 = function(e) {
+	console.log(e);
+}
+
+var fb = document.getElementById("fb");
+fb.addEventListener("click",addFib);
 
 
